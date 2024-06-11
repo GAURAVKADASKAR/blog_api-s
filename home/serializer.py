26dff2f8182a1a2cf4c29629adfc122085ba578c
new changes <kadskargaurav@gmail.com>
2024-06-11 19:09:57 +0530

    some changes

--- home/serializer.py
+++ home/serializer.py
@@ -2,6 +2,7 @@ from home.models import *
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+
 class blogserializer(serializers.ModelSerializer):
 
     class Meta:
@@ -13,5 +14,15 @@ class Userserializer(serializers.ModelSerializer):
     class Meta:
         model=User
         fields=['username','password']
+    
+    def create(self, validated_data):
+        username=validated_data['username']
+        password=validated_data['password']
+        user=User.objects.create(username=username)
+        user.set_password(password)
+        user.save()
+        return user
+    
+
 
 
